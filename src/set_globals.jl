e04f1685eb9aa748eafb2f5ce3b8fd5a14814739
Pedro Naethe <pedronaethemotta@id.uff.br>
2026-01-22 02:46:07 -0300

    Fixed some notebooks

--- src/set_globals.jl
+++ src/set_globals.jl
@@ -1,9 +1,9 @@
 
-const MODEL = "iharm" # Model type: "analytic", "thin_disk", "iharm"
+const MODEL = "analytic" # Model type: "analytic", "thin_disk", "iharm"
 
 println("Using model: $MODEL, change src/set_globals.jl to modify.")
 # Mass of the black hole in solar masses
-const MBH = 6.2e9#4.063e6 #6.2e9
+const MBH = 4.063e6#6.2e9#4.063e6 #6.2e9
 const L_unit = GNEWT * MBH * MSUN / CL^2  # Length unit in gravitational radius (Rg)
 const T_unit = L_unit / CL  # Time unit in seconds
 #const RHO_unit = M_unit / L_unit^3  # Density unit in g/cm^3
